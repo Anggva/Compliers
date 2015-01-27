f3c461a55f5bdbabc02da3c954264378ba9842ac
Vasilina Vlasova <vasenysh@gmail.com>
2015-01-27 09:00:13 -0500

    Comments work correctly, special symbols in process

--- Project1/p1.py
+++ Project1/p1.py
@@ -3,43 +3,65 @@ import sys
 
 spaces = [' ', '\n', '\t'];
 keywords = ['else', 'if','int', 'while', 'return', 'float', 'void'];
-specSymbolsG1 = ['+','-','*','/','<','=',';',',','(',')','[',']','{','}', '!', '>'];
-specSymbolsG2 = ['<','>','=','!'];
-numbers = ['0-9'];
-alphabet = ['a-zA-Z'];
+arithmetSymb = ['+','-','*','/'];
+compareSymb = ['<','>', '>=', '<=', '==', '!='];
+delimiters = [';',',','(',')','[',']','{','}']
+floatSymb = ['E', '.', '+', '-'];
+superset = arithmetSymb+compareSymb+delimiters+floatSymb
+numbers = ['0','1','2','3','4','5','6','7','8','9'];
+alphabet = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
 
 def main():
     inputFile = sys.argv[1];
     fileVar = open(inputFile, 'r');
     content = fileVar.readlines();
-    offset = -1;
     voffset = -1;
     current = 'start';
     moveToNext = 0;
     word = '';
     specWord = '';
     depth = 0;
-    for line in content:
-        for char in line:
+    i = 0;
+    while i < len(content):
+        line = content[i];
+        offset = -1;
+        j = 0;
+        while j < len(line):
+            char = line[j];
             if char=='/':
                 res = processComment(line, offset, content, voffset);
-                if res == 1:
-                    print 'INPUT: ', line[(offset+1):]
-                    offset = -1
-                    break
-                elif res == 0:
+                if res == 0:
                     pass
+                elif res == 1:
+                    current = 'comment';
+                    print 'INPUT: ', line[(offset+1):len(line)-1]
+                    break
                 elif res == [-1,-1]:
-                    print line[offset:]
+                    current = 'comment';
+                    print 'INPUT: ', line[offset:]
                     printLines(voffset, content)
                     break;
                 else:
+                    current = 'comment';
                     printLines(offset, voffset,res, content)
-                    break;
+                    if res[1] != voffset:
+                        voffset = res[1];
+                        i = voffset+1;
+                        line = content[i];
+                    if res[2] >= len(line)-1:
+                        break;
+                    else:
+                        offset = res[2];
+                        j = offset+1;
+                        char = next(line,offset);
+            if char in superset:
+                
+
             if char in alphabet:
                 if current != 'number':
                     word+=char
                     offset += 1;
+                    # j+=1;
                     current = 'word';
                 else:
                     # if char == 'E':
@@ -52,9 +74,11 @@ def main():
                     current = 'number'
                     word+=char
                     offset += 1
+                    # j+=1
             elif char in spaces:
                 offset+=1
-                if current == 'start':
+                if current == 'start' or current == 'comment':
+                    j+=1
                     continue
                 elif len(word) > 0:
                     if current == 'error':
@@ -83,7 +107,8 @@ def main():
                     specWord = ''
             elif char in specSymbolsG1:
                 current = 'spec'
-                processWord(word, depth)
+                if len(word) > 0:
+                    processWord(word, depth)
                 offset += 1;
                 word = ''
                 specWord += char
@@ -91,7 +116,9 @@ def main():
                 specWord = ''
             elif current == 'error':
                 word += char;
+            j+=1
         voffset+=1;
+        i+=1;
     fileVar.close();
 
 
@@ -111,17 +138,17 @@ def next(line, offset):
 def processWord(word, depth):
    # dicEntry='';
     if word in keywords:
-        str = 'keyword: '+ word;
+        s = 'keyword: '+ word;
     elif aNumber(word, numbers):
-        str = 'NUM: ' + word;
+        s = 'NUM: ' + word;
     else:
-        str = 'ID: ' + word + ' ' + depth;
-    print str;
+        s = 'ID: ' + word + ' ' + str(depth);
+    print s;
 
 
 def parseLine(line, offset, content, voffset, c1, c2):
     cDepth = 1;
-    i = offset + 3
+    i = offset + 3; #Skip /*
     while i < len(line):
         if line[i] == c2 and line[i+1] == c1:
             cDepth += 1;
@@ -130,19 +157,24 @@ def parseLine(line, offset, content, voffset, c1, c2):
         elif line[i] == c1 and line[i+1] == c2:
             cDepth -= 1;
             if cDepth == 0:
-                return [i, voffset];
+                return [offset, voffset, i+2];
             i+=1;
         i+=1;
+
     for i in range (voffset+2, len(content)):
         line = content[i];
-        for j in range (0, len(line)-1):
+        j = 0;
+        while j < len(line):
             if line[j] == c2 and line[j+1] == c1:
                 cDepth += 1;
+                j+=2
                 continue;
             elif line[j] == c1 and line[j+1] == c2:
                 cDepth -= 1;
                 if cDepth == 0:
-                    return [j, i];
+                    return [offset,i-1,j+2];
+                j+=1;
+            j+=1
     return [-1,-1];
 
 def processComment(line, offset, content, voffset):
@@ -161,27 +193,28 @@ def aNumber(line, numbers):
     return 1;
 def printLines(voffset, content):
     for i in range (voffset+2, len(content)):
-        print content[i]
+        print content[i][0:]
 
 def printLines(offset, voffset, res, content):
-    ln = res[0] - voffset - 2;
-    if ln == -1:
-        line = content[res[0]]
-        print line[offset+2:res[1]]
-    elif ln == 0:
-        line = content[voffset+2]
-        print line[offset+2:]
-        line = content[res[0]]
-        print line[:res[1]]
+    ln = res[1] - voffset;
+    if ln == 0:
+        line = content[res[1]+1]
+        print 'INPUT: ', line[res[0]+1:res[2]]
+    elif ln == 1:
+        line = content[voffset+1]
+        print 'INPUT: ', line[res[0]+1:len(line)-1]
+        line = content[res[1]+1]
+        print 'INPUT: ', line[:res[2]]
     else:
         i = voffset+1
         line = content[i]
         print 'INPUT: ', line[offset+1:]
+        i+=1
         while i<res[1]:
             line = content[i]
-            print 'INPUT: ', line
+            print 'INPUT: ', line[:len(line)-1]
             i+=1
         line = content[res[1]]
-        print line[:res[1]]
+        print line[:res[2]]
 
 main();
